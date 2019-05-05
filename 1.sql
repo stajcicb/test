e980267586b630fef90d2d8ef6848db11d62dd07
stajcicb <48075008+stajcicb@users.noreply.github.com>
2019-05-05 12:37:08 +0200

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,25 +1,23 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- version 4.1.14
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Apr 27, 2018 at 11:13 AM
--- Server version: 5.7.19
--- PHP Version: 5.6.31
+-- Host: 127.0.0.1
+-- Generation Time: Apr 26, 2018 at 02:09 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Database: `etf_psi2018_k2`
+-- Database: `etfbaza-2018k2-drazen`
 --
 
 -- --------------------------------------------------------
@@ -28,7 +26,6 @@ SET time_zone = "+00:00";
 -- Table structure for table `drzi_predmet`
 --
 
-DROP TABLE IF EXISTS `drzi_predmet`;
 CREATE TABLE IF NOT EXISTS `drzi_predmet` (
   `korisnickoime` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
   `sifrapredmet` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
@@ -41,6 +38,7 @@ CREATE TABLE IF NOT EXISTS `drzi_predmet` (
 --
 
 INSERT INTO `drzi_predmet` (`korisnickoime`, `sifrapredmet`) VALUES
+('nastavnik', 'si3psi'),
 ('bojic', 'si3psi'),
 ('draskovic', 'si3psi'),
 ('sekularac', 'si3psi'),
@@ -54,15 +52,37 @@ INSERT INTO `drzi_predmet` (`korisnickoime`, `sifrapredmet`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `nastavnik`
+-- Table structure for table `korisnik`
 --
 
-DROP TABLE IF EXISTS `nastavnik`;
-CREATE TABLE IF NOT EXISTS `nastavnik` (
+CREATE TABLE IF NOT EXISTS `korisnik` (
   `username` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
   `ime` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `prezime` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `korisnik`
+--
+
+INSERT INTO `korisnik` (`username`, `password`, `ime`, `prezime`, `email`) VALUES
+('nastavnik', 'sifra321', 'Nastavnik', 'Nastavnikovic', 'nastavnik@etf.rs'),
+('bojic', 'dragan123', 'Dragan', 'Bojic', 'bojic@etf.rs'),
+('draskovic', 'drasko123', 'Dražen', 'Drašković', 'draskovic@etf.rs'),
+('nbosko', 'bosko123', 'Boško', 'Nikolić', 'nbosko@etf.rs'),
+('sekularac', 'tamara321', 'Tamara', 'Šekularac', 'tasha@etf.rs');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nastavnik`
+--
+
+CREATE TABLE IF NOT EXISTS `nastavnik` (
+  `username` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
   `titula` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
   `zvanje` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`username`),
@@ -73,11 +93,12 @@ CREATE TABLE IF NOT EXISTS `nastavnik` (
 -- Dumping data for table `nastavnik`
 --
 
-INSERT INTO `nastavnik` (`username`, `password`, `ime`, `prezime`, `titula`, `zvanje`) VALUES
-('bojic', 'dragan123', 'Dragan', 'Bojic', 'Dr', 'vanredni prof.'),
-('draskovic', 'drazen123', 'Drazen', 'Draskovic', 'MSc', 'asistent u nastavi'),
-('nbosko', 'bosko123', 'Bosko', 'Nikolic', 'Dr', 'redovni prof.'),
-('sekularac', 'tasha123', 'Tamara', 'Sekularac', 'BSc', 'saradnik u nastavi');
+INSERT INTO `nastavnik` (`username`, `titula`, `zvanje`) VALUES
+('nastavnik', 'Dr', 'redovni prof.'),
+('bojic', 'Dr', 'vanredni prof.'),
+('draskovic', 'MSc', 'asistent u nastavi'),
+('nbosko', 'Dr', 'redovni prof.'),
+('sekularac', 'BSc', 'saradnik u nastavi');
 
 -- --------------------------------------------------------
 
@@ -85,26 +106,19 @@ INSERT INTO `nastavnik` (`username`, `password`, `ime`, `prezime`, `titula`, `zv
 -- Table structure for table `polaganje`
 --
 
-DROP TABLE IF EXISTS `polaganje`;
 CREATE TABLE IF NOT EXISTS `polaganje` (
   `idprijave` int(11) NOT NULL AUTO_INCREMENT,
   `user_student` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
   `user_nastavnik` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
   `sifra_predmet` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
+  `datum_unosa` date NOT NULL,
   `ocena` int(11) NOT NULL,
+  `ponistio` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`idprijave`),
   KEY `user_student` (`user_student`),
   KEY `user_nastavnik` (`user_nastavnik`),
   KEY `sifra_predmet` (`sifra_predmet`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `polaganje`
---
-
-INSERT INTO `polaganje` (`idprijave`, `user_student`, `user_nastavnik`, `sifra_predmet`, `ocena`) VALUES
-(1, 'jova', 'bojic', 'si3psi', 9),
-(2, 'petar', 'draskovic', 'si3psi', 8);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -112,7 +126,6 @@ INSERT INTO `polaganje` (`idprijave`, `user_student`, `user_nastavnik`, `sifra_p
 -- Table structure for table `predmet`
 --
 
-DROP TABLE IF EXISTS `predmet`;
 CREATE TABLE IF NOT EXISTS `predmet` (
   `sifra` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
   `naziv` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -137,31 +150,30 @@ INSERT INTO `predmet` (`sifra`, `naziv`, `ESPB`, `tip`, `semestar`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `slusa_predmet`
+--
+
+CREATE TABLE IF NOT EXISTS `slusa_predmet` (
+  `korisnickoime` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
+  `sifrapredmet` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`korisnickoime`,`sifrapredmet`),
+  KEY `sifrapredmet` (`sifrapredmet`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `student`
 --
 
-DROP TABLE IF EXISTS `student`;
 CREATE TABLE IF NOT EXISTS `student` (
   `username` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
-  `ime` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `prezime` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `godina_upisa` int(11) NOT NULL,
   `broj_indeksa` int(11) NOT NULL,
   `odsek` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`username`),
   KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `student`
---
-
-INSERT INTO `student` (`username`, `password`, `ime`, `prezime`, `godina_upisa`, `broj_indeksa`, `odsek`) VALUES
-('jova', 'sifra123', 'Jovan', 'Jovanovic', 2016, 133, 'SI'),
-('jovana', 'sifra123', 'Jovana', 'Jovanovic', 2015, 122, 'RTI'),
-('milana', 'sifra123', 'Milana', 'Milanovic', 2016, 244, 'RTI'),
-('petar', 'sifra123', 'Petar', 'Petrovic', 2015, 396, 'SI');
 
 --
 -- Constraints for dumped tables
@@ -171,17 +183,35 @@ INSERT INTO `student` (`username`, `password`, `ime`, `prezime`, `godina_upisa`,
 -- Constraints for table `drzi_predmet`
 --
 ALTER TABLE `drzi_predmet`
-  ADD CONSTRAINT `drzi_predmet_ibfk_1` FOREIGN KEY (`korisnickoime`) REFERENCES `nastavnik` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `drzi_predmet_ibfk_2` FOREIGN KEY (`sifrapredmet`) REFERENCES `predmet` (`sifra`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `drzi_predmet_ibfk_2` FOREIGN KEY (`sifrapredmet`) REFERENCES `predmet` (`sifra`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `drzi_predmet_ibfk_1` FOREIGN KEY (`korisnickoime`) REFERENCES `korisnik` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `nastavnik`
+--
+ALTER TABLE `nastavnik`
+  ADD CONSTRAINT `nastavnik_ibfk_1` FOREIGN KEY (`username`) REFERENCES `korisnik` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `polaganje`
 --
 ALTER TABLE `polaganje`
+  ADD CONSTRAINT `polaganje_ibfk_3` FOREIGN KEY (`sifra_predmet`) REFERENCES `predmet` (`sifra`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `polaganje_ibfk_1` FOREIGN KEY (`user_student`) REFERENCES `student` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `polaganje_ibfk_2` FOREIGN KEY (`user_nastavnik`) REFERENCES `nastavnik` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `polaganje_ibfk_3` FOREIGN KEY (`sifra_predmet`) REFERENCES `predmet` (`sifra`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
+  ADD CONSTRAINT `polaganje_ibfk_2` FOREIGN KEY (`user_nastavnik`) REFERENCES `nastavnik` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `slusa_predmet`
+--
+ALTER TABLE `slusa_predmet`
+  ADD CONSTRAINT `slusa_predmet_ibfk_2` FOREIGN KEY (`sifrapredmet`) REFERENCES `predmet` (`sifra`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `slusa_predmet_ibfk_1` FOREIGN KEY (`korisnickoime`) REFERENCES `korisnik` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `student`
+--
+ALTER TABLE `student`
+  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`username`) REFERENCES `korisnik` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
